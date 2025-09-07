60b73d898913986a1fd6894617ffccf61088fb57
860607 <lebibi001@yahoo.fr>
2025-09-07 12:38:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,16 @@
 
-const CACHE = 'sos-monde-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/assets/css/styles.css',
-  '/assets/js/app.js',
-  '/assets/js/i18n.js',
-  '/assets/img/icon-192.png',
-  '/assets/img/icon-512.png',
-  '/manifest.json'
+const CACHE='sos-monde-v1';
+const ASSETS=[
+  'index.html',
+  'assets/css/styles.css',
+  'assets/js/app.js',
+  'assets/js/i18n.js',
+  'assets/js/emergency.js',
+  'assets/js/country-flags.js',
+  'assets/img/icon-192.png',
+  'assets/img/icon-512.png',
+  'manifest.json'
 ];
-
-self.addEventListener('install', event=>{
-  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE && caches.delete(k))))
-  );
-});
-
-self.addEventListener('fetch', event=>{
-  event.respondWith(
-    caches.match(event.request).then(resp=> resp || fetch(event.request))
-  );
-});
+self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
+self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE && caches.delete(k))))); });
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
